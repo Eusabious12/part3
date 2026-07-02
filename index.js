dd1b737cf672978ec50d94b2b20dd076f60c8244
Eusabious <Eusabious@MacBook-Air.local>
2026-07-02 22:37:45 +0300

    Exercises 3.7-3.8: morgan logging with request body

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
-// phonebook data
+// phonebook data. 
 let phonebook = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
